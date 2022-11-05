97df63c97814584a4a535781bde1d308822c576f
Yusuke Kawasaki <u-suke@kawa.net>
2022-11-05 15:05:54 +0900

    multiple ports --config=first.yml,second.yml,third.yml

--- bin/weboverlay.cli.ts
+++ bin/weboverlay.cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as fs from "fs";
+import {promises as fs} from "fs";
 import * as YAML from "yaml";
 
 const argv = require("process.argv")(process.argv.slice(2));
@@ -9,6 +9,7 @@ import {weboverlay, WebOverlayOptions} from "../";
 
 interface CLIOptions extends WebOverlayOptions {
     config?: string;
+    "--"?: string[];
 }
 
 const defaults: WebOverlayOptions = {
@@ -24,19 +25,46 @@ async function CLI(args: CLIOptions) {
     // Basic authentication
     if ("string" === typeof basic) options.basic = basic.split(",");
 
-    options.layers = (args as any)["--"] || [];
+    // Layers
+    const layers = args["--"];
+    if (layers?.length) options.layers = layers;
 
-    // --config=weboverlay.yml
-    if (config) {
-        const yaml = await fs.promises.readFile(config, "utf-8");
-        const data = YAML.parse(yaml) as CLIOptions;
-        Object.keys(data).forEach((key: keyof CLIOptions) => (options as any)[key] = data[key]);
+    // WITHOUT --config=
+    if (!config) {
+        weboverlay(merge(defaults, options));
+        return;
     }
 
-    weboverlay(options);
+    // WITH --config=weboverlay.yml
+    // OR   --config=first.yml,second.yml,third.yml
+    const files = config.split(",");
+    for (const file of files) {
+        const yaml = await fs.readFile(file, "utf-8");
+        const data = YAML.parse(yaml) as CLIOptions;
+        const merged = merge(defaults, data, options);
+
+        if (files.length > 1 && !merged.logfile && merged.port) {
+            const log = (message: string) => console.log(`[${merged.port}] ${message}`);
+            merged.logger = {log};
+        }
+
+        weboverlay(merged);
+    }
 }
 
-CLI(argv(defaults)).catch(fatal);
+function merge(...options: WebOverlayOptions[]): WebOverlayOptions {
+    let option: WebOverlayOptions = {};
+
+    for (const opt of options) {
+        Object.keys(opt).forEach((key: keyof WebOverlayOptions) => {
+            if (opt[key] !== undefined) option[key] = opt[key];
+        });
+    }
+
+    return option;
+}
+
+CLI(argv()).catch(fatal);
 
 function fatal(e: Error | any) {
     if (e) console.warn(e.message || e);
